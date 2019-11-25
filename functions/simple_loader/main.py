a7e6fe19c87d933629b99f574f290eefd8a6280d
abalogh2 <adam.balogh2@emarsys.com>
2019-11-25 18:36:25 +0100

    making simple loader a single file function

--- functions/simple_loader/main.py
+++ functions/simple_loader/main.py
@@ -2,33 +2,44 @@ import logging
 import os
 
 from google.cloud import bigquery
-from google.cloud.bigquery import LoadJobConfig, TimePartitioning, CreateDisposition, WriteDisposition
+from google.cloud.bigquery import LoadJobConfig, TimePartitioning, CreateDisposition, WriteDisposition, SchemaField
 
 PROJECT_ID = os.getenv('GCP_PROJECT')
 CLIENT = bigquery.Client(PROJECT_ID)
 DATASET = 'sales_data'
 TABLE = 'customer_123'
 JOB_ID_PREFIX = 'simple_loader_'
-SCHEMA = CLIENT.schema_from_json('sales_data_schema.json')
 CLUSTERING_FIELDS = ['contact_id']
+SCHEMA_DICT = [
+    {"mode": "NULLABLE", "name": "contact_id", "type": "INTEGER"},
+    {"mode": "NULLABLE", "name": "event_time", "type": "TIMESTAMP"},
+    {"mode": "NULLABLE", "name": "order_id", "type": "INTEGER"},
+    {"mode": "NULLABLE", "name": "item_id", "type": "INTEGER"},
+    {"mode": "NULLABLE", "name": "quantity", "type": "FLOAT"},
+    {"mode": "NULLABLE", "name": "price", "type": "FLOAT"}
+]
 
 
 def load_from_gcs(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    uri = f"gs://{event['bucket']}/{event['name']}"
-    destination_table = CLIENT.dataset(DATASET).table(TABLE)
-    job_config = create_job_config()
     try:
-        job = CLIENT.load_table_from_uri(uri, destination_table, job_id_prefix=JOB_ID_PREFIX, job_config=job_config)
+        uri = f"gs://{event['bucket']}/{event['name']}"
+        destination_table = CLIENT.dataset(DATASET).table(TABLE)
+        job_config = create_job_config()
+
+        job = CLIENT.load_table_from_uri(source_uris=uri,
+                                         destination=destination_table,
+                                         job_id_prefix=JOB_ID_PREFIX,
+                                         job_config=job_config)
+
         logging.info(f"Started job {job.job_id}")
-        result = job.result()
+        job.result()
         logging.info(f"Processed {uri}")
     except Exception as err:
         logging.error(f"Error happened during processing of {uri}: {str(err)}")
+
+
+def create_schema_for_table():
+    return [SchemaField.from_api_repr(field) for field in SCHEMA_DICT]
 
 
 def create_job_config():
@@ -36,10 +47,7 @@ def create_job_config():
     config.skip_leading_rows = 1
     config.time_partitioning = TimePartitioning()
     config.clustering_fields = CLUSTERING_FIELDS
-    config.schema = SCHEMA
+    config.schema = create_schema_for_table()
     config.create_disposition = CreateDisposition.CREATE_IF_NEEDED
     config.write_disposition = WriteDisposition.WRITE_APPEND
     return config
-
-
-
