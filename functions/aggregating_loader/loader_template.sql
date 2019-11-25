7bdb0a1bd3a7486d387340f9496beab82d151c24
abalogh2 <adam.balogh2@emarsys.com>
2019-11-25 17:05:30 +0100

    creating aggregating loader

---
+++ functions/aggregating_loader/loader_template.sql
@@ -0,0 +1,13 @@
+SELECT
+    contact_id
+    , event_time
+    , order_id
+    , ARRAY_AGG(
+        STRUCT(
+            item_id
+            , quantity
+            , price
+        )
+    ) AS items
+FROM {EXTERNAL_TABLE_NAME}
+GROUP BY 1,2,3
---
+++ functions/aggregating_loader/main.py
@@ -0,0 +1,57 @@
+import logging
+import os
+
+from google.cloud import bigquery
+from google.cloud.bigquery import TimePartitioning, CreateDisposition, WriteDisposition, QueryJobConfig, \
+    ExternalConfig, ExternalSourceFormat
+
+PROJECT_ID = os.getenv('GCP_PROJECT')
+CLIENT = bigquery.Client(PROJECT_ID)
+DATASET = 'sales_data_aggregated'
+TABLE = 'customer_123'
+JOB_ID_PREFIX = 'aggregating_loader_'
+SCHEMA = CLIENT.schema_from_json('sales_data_schema.json')
+CLUSTERING_FIELDS = ['contact_id']
+QUERY_TEMPLATE_FILE = 'loader_template.sql'
+EXTERNAL_TABLE_NAME_IN_QUERY = 'sales_data_external_table'
+COMPRESSION = 'GZIP'
+
+
+def load_from_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    uri = f"gs://{event['bucket']}/{event['name']}"
+    destination_table = CLIENT.dataset(DATASET).table(TABLE)
+    job_config = create_job_config(uri, destination_table)
+    try:
+        with open(QUERY_TEMPLATE_FILE) as query_template_file:
+            query_template = query_template_file.read()
+        job = CLIENT.query(query=query_template.format(EXTERNAL_TABLE_NAME=EXTERNAL_TABLE_NAME_IN_QUERY),
+                           job_config=job_config,
+                           job_id_prefix=JOB_ID_PREFIX)
+        logging.info(f"Started job {job.job_id}")
+        result = job.result()
+        logging.info(f"Processed {uri}")
+    except Exception as err:
+        logging.error(f"Error happened during processing of {uri}: {str(err)}")
+
+
+def create_job_config(external_source_uri, destination_table):
+    config = QueryJobConfig()
+    config.create_disposition = CreateDisposition.CREATE_IF_NEEDED
+    config.write_disposition = WriteDisposition.WRITE_APPEND
+    config.clustering_fields = CLUSTERING_FIELDS
+    config.time_partitioning = TimePartitioning()
+    config.destination = destination_table
+
+    external_config = ExternalConfig(ExternalSourceFormat.CSV)
+    external_config.schema = SCHEMA
+    external_config.source_uris = external_source_uri
+    external_config.compression = COMPRESSION
+    external_config.options.skip_leading_rows = 1
+
+    config.table_definitions = {EXTERNAL_TABLE_NAME_IN_QUERY: external_config}
+    return config
